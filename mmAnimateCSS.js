9aa794aa16b5b33bc8f2e53cd71b1fef269cc423
RubyLouvre <cheng19840218@gmail.com>
2014-11-26 20:15:14 +0800

    update

--- mmAnimateCSS.js
+++ mmAnimateCSS.js
@@ -312,7 +312,7 @@ define(["avalon"], function() {
                 var rule = cssRules[i];
                 if (rule[prop] === ruleName) {
                     sheet.deleteRule(i);
-                    //    avalon.log("已经成功删除" + name + " " + ruleName);
+                    avalon.log("已经成功删除" + name + " " + ruleName);
                     break;
                 }
             }
@@ -333,7 +333,7 @@ define(["avalon"], function() {
         this.count = 1
         this.playState = true //是否能更新
     }
-
+    var $playState = avalon.cssName("animation-play-state")
 
     Frame.prototype = {
         constructor: Frame,
@@ -348,6 +348,10 @@ define(["avalon"], function() {
             for (var i = 0, fn; fn = fns[i++]; ) {
                 fn.call(this.elem, args)
             }
+        },
+        play: function(stop) {
+            this.elem.style[$playState] = stop ? "paused" : "running"
+            this.paused = stop ? Date.now() : null
         },
         build: function() {
             var frame = this
@@ -568,6 +572,125 @@ define(["avalon"], function() {
             set: function(tween) {
                 tween.elem[tween.name] = tween.now
             }
+        }
+    })
+
+    /*********************************************************************
+     *                                  原型方法                            *
+     **********************************************************************/
+
+    avalon.fn.mix({
+        delay: function(ms) {
+            return this.animate(ms)
+        },
+        pause: function() {
+            var cur = this[0]
+            for (var i = 0, frame; frame = timeline[i]; i++) {
+                if (frame.elem === cur) {
+                    frame.play("stop")
+                }
+            }
+            return this
+        },
+        resume: function() {
+            var now = new Date
+            var elem = this[0]
+            for (var i = 0, frame; frame = timeline[i]; i++) {
+                if (frame.elem === elem) {
+                    frame.startTime += (now - frame.paused)
+                    delete frame.paused
+                }
+            }
+            return this
+        },
+        //如果clearQueue为true，是否清空列队
+        //如果gotoEnd 为true，是否跳到此动画最后一帧
+        stop: function(clearQueue, gotoEnd) {
+            clearQueue = clearQueue ? "1" : ""
+            gotoEnd = gotoEnd ? "1" : "0"
+            var stopCode = parseInt(clearQueue + gotoEnd, 2) //返回0 1 2 3
+            var node = this[0]
+            for (var i = 0, frame; frame = timeline[i]; i++) {
+                if (frame.elem === node) {
+                    frame.gotoEnd = true
+                    switch (stopCode) { //如果此时调用了stop方法
+                        case 0:
+                            // false false 中断当前动画，继续下一个动画
+                            frame.playState = frame.revert = false
+                            break
+                        case 1:
+                            // false true立即跳到最后一帧，继续下一个动画
+                            frame.revert = false
+                            break
+                        case 2:
+                            // true false清空该元素的所有动画
+                            delete frame.elem
+                            break
+                        case 3:
+                            // true true 立即完成该元素的所有动画
+                            frame.troops.forEach(function(a) {
+                                a.gotoEnd = true
+                            })
+                            break
+                    }
+                }
+            }
+            return this
+        }
+    })
+    /*********************************************************************
+     *                                 常用特效                            *
+     **********************************************************************/
+    var fxAttrs = [
+        ["height", "marginTop", "marginBottom", "borderTopWidth", "borderBottomWidth", "paddingTop", "paddingBottom"],
+        ["width", "marginLeft", "marginRight", "borderLeftWidth", "borderRightWidth", "paddingLeft", "paddingRight"],
+        ["opacity"]
+    ]
+    function genFx(type, num) { //生成属性包
+        var obj = {}
+        fxAttrs.concat.apply([], fxAttrs.slice(0, num)).forEach(function(name) {
+            obj[name] = type
+            if (~name.indexOf("margin")) {
+                Tween.propHooks[name] = {
+                    get: Tween.propHooks._default.get,
+                    set: function(tween) {
+                        tween.elem.style[tween.name] = Math.max(tween.now, 0) + tween.unit
+                    }
+                }
+            }
+        })
+        return obj
+    }
+
+
+    var effects = {
+        slideDown: genFx("show", 1),
+        slideUp: genFx("hide", 1),
+        slideToggle: genFx("toggle", 1),
+        fadeIn: {
+            opacity: "show"
+        },
+        fadeOut: {
+            opacity: "hide"
+        },
+        fadeToggle: {
+            opacity: "toggle"
+        }
+    }
+
+    avalon.each(effects, function(method, props) {
+        avalon.fn[method] = function() {
+            props.frameName = method
+            var args = [].concat.apply([props], arguments)
+            return this.animate.apply(this, args)
+        }
+    })
+
+    String("toggle,show,hide").replace(avalon.rword, function(name) {
+        avalon.fn[name] = function() {
+            var args = [].concat.apply([genFx(name, 3)], arguments)
+            args[0].frameName = name
+            return this.animate.apply(this, args)
         }
     })
     return avalon
